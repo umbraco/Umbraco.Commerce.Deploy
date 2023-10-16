1e9ef73f69360b9837e28d7130dac7792ac28199
Robert Foster <robert@youritgroup.com.au>
2023-10-16 13:36:30 +1100

    Fixes configuration issue for Umbraco 12.2.0 when attempting to publish content nodes with StoreEntityPicker values #1

--- src/Umbraco.Commerce.Deploy/Connectors/ValueConnectors/UmbracoCommerceStoreEntityPickerValueConnector.cs
+++ src/Umbraco.Commerce.Deploy/Connectors/ValueConnectors/UmbracoCommerceStoreEntityPickerValueConnector.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using Umbraco.Commerce.Core.Api;
-using Umbraco.Commerce.Core.Models;
-using Umbraco.Commerce.Deploy.Configuration;
-
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Deploy;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Commerce.Cms.PropertyEditors.StorePicker;
+using Umbraco.Commerce.Core.Api;
+using Umbraco.Commerce.Core.Models;
+using Umbraco.Commerce.Deploy.Configuration;
 using Umbraco.Extensions;
 
 namespace Umbraco.Commerce.Deploy.Connectors.ValueConnectors
@@ -69,31 +69,25 @@ namespace Umbraco.Commerce.Deploy.Connectors.ValueConnectors
         {
             var dataType = _dataTypeService.GetDataType(propertyType.DataTypeId);
 
-            var cfg = dataType.ConfigurationAs<Dictionary<string, object>>();
-
-            if (cfg.ContainsKey("entityType"))
+            var cfg = dataType.ConfigurationAs<StoreEntityPickerConfiguration>();
+            switch (cfg.EntityType)
             {
-                var entityType = cfg["entityType"]?.ToString();
-
-                switch (entityType)
-                {
-                    case "OrderStatus":
-                        return UmbracoCommerceConstants.UdiEntityType.OrderStatus;
-                    case "Country":
-                        return UmbracoCommerceConstants.UdiEntityType.Country;
-                    case "ShippingMethod":
-                        return UmbracoCommerceConstants.UdiEntityType.ShippingMethod;
-                    case "PaymentMethod":
-                        return UmbracoCommerceConstants.UdiEntityType.PaymentMethod;
-                    case "Currency":
-                        return UmbracoCommerceConstants.UdiEntityType.Currency;
-                    case "TaxClass":
-                        return UmbracoCommerceConstants.UdiEntityType.TaxClass;
-                    case "EmailTemplate":
-                        return UmbracoCommerceConstants.UdiEntityType.EmailTemplate;
-                    case "Discount": // Not sure if discounts should transfer as these are "user generated"
-                        return UmbracoCommerceConstants.UdiEntityType.Discount;
-                }
+                case "OrderStatus":
+                    return UmbracoCommerceConstants.UdiEntityType.OrderStatus;
+                case "Country":
+                    return UmbracoCommerceConstants.UdiEntityType.Country;
+                case "ShippingMethod":
+                    return UmbracoCommerceConstants.UdiEntityType.ShippingMethod;
+                case "PaymentMethod":
+                    return UmbracoCommerceConstants.UdiEntityType.PaymentMethod;
+                case "Currency":
+                    return UmbracoCommerceConstants.UdiEntityType.Currency;
+                case "TaxClass":
+                    return UmbracoCommerceConstants.UdiEntityType.TaxClass;
+                case "EmailTemplate":
+                    return UmbracoCommerceConstants.UdiEntityType.EmailTemplate;
+                case "Discount": // Not sure if discounts should transfer as these are "user generated"
+                    return UmbracoCommerceConstants.UdiEntityType.Discount;
             }
 
             return null;
